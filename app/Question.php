72c1dd0a9edc78df090ed8d27d20079fa00b00e6
bigidici <bambagdc@gmail.com>
2020-03-08 19:57:03 +0000

    Prepupulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App;
-
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = ['title','body'];
+
     public function user() {
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
     }
-    
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
--- database/migrations/2020_03_07_161219_create_questions_table.php
+++ database/migrations/2020_03_07_161219_create_questions_table.php
@@ -14,18 +14,20 @@ class CreateQuestionsTable extends Migration
     public function up()
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('body');
             $table->unsignedInteger('views')->default(0);
             $table->unsignedInteger('answers')->default(0);
-            $table->Integer('votes')->default(0);
+            $table->integer('votes')->default(0);
             $table->unsignedInteger('best_answer_id')->nullable();
             $table->unsignedInteger('user_id');
             $table->timestamps();
-            $table->foreign('user_id')-> references('id')->on('users')->onDelete('cascade');
+
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
+        
     }
 
     /**
